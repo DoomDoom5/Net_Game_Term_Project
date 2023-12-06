d8d30f179c4d9a9b831143a4e9d5873501031d92
dhc518 <dhc000518@gmail.com>
2023-12-06 22:48:25 +0900

    총 각도 설정

--- Server_TermProject_ComputerGraphics-main/src/Player.cpp
+++ Server_TermProject_ComputerGraphics-main/src/Player.cpp
@@ -369,7 +369,8 @@ GLvoid Player::ChangeState(const State& playerState, const Event& e, const GLint
 GLvoid Player::Update()
 {
 	mCrntState->Update();
-	
+	mCrntGun->SetYaw(mYaw);
+	mCrntGun->SetPitch(mPitch);
 	if (mlsFire) mCrntGun->StartFire();
 	else mCrntGun->StopFire();
 	mPosition = mBody->GetPviotedPosition();
